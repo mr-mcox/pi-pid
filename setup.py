1a9e3064bbd067aea0a5d159fa1eda7e358251eb
Matthew Cox <mr.mcox@gmail.com>
2018-03-10 16:45:12 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pi-pid',
+    version='0.0.1',
+    author='Matthew Cox',
+    author_email='mr.mcox@gmail.com',
+    package_dir={'': 'src'},
+    install_requires=['click', 'attrs'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    packages=find_packages(where='src'),
+)
