5fe2ed7dffb88eeb386a2219c20fa40c9158beb7
Matthew Cox <mr.mcox@gmail.com>
2018-03-15 10:43:52 -0500

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pi_pid',
-    version='0.3.1',
+    version='0.3.2',
     author='Matthew Cox',
     author_email='mr.mcox@gmail.com',
     package_dir={'': 'src'},
